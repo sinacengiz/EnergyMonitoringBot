117f96e30279134166d9cc849a26f79381cf681f
sina cengiz <33090337+sinacengiz@users.noreply.github.com>
2020-05-01 15:43:27 +0300

    Add files via upload

--- OSOS.py
+++ OSOS.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 import time
-browser = webdriver.Chrome()
-browser.get("https://osos.akdenizedas.com.tr/osos/login.iface")
-enter_user=browser.find_element_by_id("frmLoginPanel:inpUser")
-enter_pswrd=browser.find_element_by_id("frmLoginPanel:inpPass")
-enter_btn=browser.find_element_by_id("frmLoginPanel:cmdBtnValidate")
-enter_user.send_keys("sinacengiz@gmail.com")
-enter_pswrd.send_keys("maverick1")
-enter_btn.click()
-time.sleep(100)
-browser.close()
+class EnergyAutomation():
+    def __init__(self):
+        browser = webdriver.Chrome()
+        browser.get("https://osos.akdenizedas.com.tr/osos/login.iface")
+        enter_user = browser.find_element_by_id("frmLoginPanel:inpUser")
+        enter_pswrd = browser.find_element_by_id("frmLoginPanel:inpPass")
+        enter_btn = browser.find_element_by_id("frmLoginPanel:cmdBtnValidate")
+        enter_user.send_keys("sinacengiz@gmail.com")
+        enter_pswrd.send_keys("maverick1")
+        enter_btn.click()
+        time.sleep(100)
+        browser.close()
+energy_aut = EnergyAutomation()
