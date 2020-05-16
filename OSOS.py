5be365a43b491ab45378d3e5ea019fa2f91b036b
sina cengiz <33090337+sinacengiz@users.noreply.github.com>
2020-05-17 00:33:21 +0300

    Update OSOS.py

--- OSOS.py
+++ OSOS.py
@@ -14,19 +14,22 @@ class EnergyAutomation():
         enter_user = browser.find_element_by_id("frmLoginPanel:inpUser")
         enter_pswrd = browser.find_element_by_id("frmLoginPanel:inpPass")
         enter_btn = browser.find_element_by_id("frmLoginPanel:cmdBtnValidate")
-        enter_user.send_keys("sinacengiz@gmail.com")
-        enter_pswrd.send_keys("maverick1")
+        enter_user.send_keys("2687521000")
+        enter_pswrd.send_keys("Aa.010420")
         enter_btn.click()
-        time.sleep(100)
+        time.sleep(10)
+        click_btn=browser.find_element_by_class_name("iceDatTblCol2")
+        click_btn.click()
+        time.sleep(1000)
     def get_data(self):
-        page=requests.get(browser.current_url)
-        soup=BeautifulSoup(page.content,"html.parser")
+        self.page=requestgits.get(browser.current_url)
+        soup=BeautifulSoup(self.page.content,"html.parser")
         data = soup.find_all("table", {"class": "chart full-width"})
         browser.close()
     def graph(self):
         pass
     def notify(self):
         pass
-    #use whatsapp API to notify
+
     
 energy_aut = EnergyAutomation()
